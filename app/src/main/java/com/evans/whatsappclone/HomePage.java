be07fe1f7d92f4fb12e02a52ea09cd141a98445d
Evans Shango <lutatwaevans@gmail.com>
2020-01-04 18:37:36 +0300

    add sign in with phone number

--- app/src/main/java/com/evans/whatsappclone/HomePage.java
+++ app/src/main/java/com/evans/whatsappclone/HomePage.java
@@ -2,13 +2,39 @@ package com.evans.whatsappclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomePage extends AppCompatActivity {
+
+    private static final String TAG = "HomePage";
+    private FirebaseAuth mAuth;
+    private Button mLogOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        initViews();
+
+        mLogOut.setOnClickListener(v -> doLogOut());
+    }
+
+    private void doLogOut() {
+        mAuth.signOut();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+    }
+
+    private void initViews() {
+        mLogOut = findViewById(R.id.btnLogOut);
     }
 }
--- app/src/main/java/com/evans/whatsappclone/MainActivity.java
+++ app/src/main/java/com/evans/whatsappclone/MainActivity.java
@@ -1,19 +1,16 @@
 package com.evans.whatsappclone;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseException;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
@@ -28,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private Button mVerify;
     private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
     private FirebaseAuth mAuth;
+    private String mVerificationId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,12 +39,26 @@ public class MainActivity extends AppCompatActivity {
 
         initViews();
 
-        mVerify.setOnClickListener(v -> startPhoneVerification());
+        mVerify.setOnClickListener(v -> {
+            if (mVerificationId != null)
+                verifyPhoneWithCode();
+            else
+                startPhoneVerification();
+
+        });
 
         mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
             @Override
             public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
                 signInWithPhoneAuthCredential(phoneAuthCredential);
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken
+                    forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                mVerificationId = s;
+                mVerify.setText(R.string.verify_code);
             }
 
             @Override
@@ -56,9 +68,16 @@ public class MainActivity extends AppCompatActivity {
         };
     }
 
+    private void verifyPhoneWithCode() {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(
+                mVerificationId, mCode.getText().toString()
+        );
+        signInWithPhoneAuthCredential(credential);
+    }
+
     private void signInWithPhoneAuthCredential(PhoneAuthCredential phoneAuthCredential) {
         mAuth.signInWithCredential(phoneAuthCredential).addOnCompleteListener(task -> {
-            if (task.isSuccessful()){
+            if (task.isSuccessful()) {
                 userIsLoggedIn();
             }
         });
@@ -66,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void userIsLoggedIn() {
         FirebaseUser user = mAuth.getCurrentUser();
-        if (user != null){
+        if (user != null) {
             startActivity(new Intent(this, HomePage.class));
             finish();
         }
